c6a3080cd0c04da7ec1b8592853d7a5cf9264f5a
twolowiec <t.wolowiec@op.pl>
2021-07-18 16:57:03 +0200

    more enum fixes

--- src/com/company/actors/Customer.java
+++ src/com/company/actors/Customer.java
@@ -1,10 +1,15 @@
 package com.company.actors;
 
+import com.company.game.EnumData;
+
 public class Customer {
     public String firstName;
     public String lastName;
     public String[] brandPreference; // ulubione marki - tablica 2 obiektów
     public Double money;
+    EnumData.VechicleType vechicleType;
+    EnumData.CustomerVechicleCondition vechicleCondition;
+
     // TODO tolerancja uszkodzeń Sprawny/zawiecha/coś innego
     // TODO czy chce dostawczy
     // TODO preferencje przestrzeni ładunkowej
--- src/com/company/game/EnumData.java
+++ src/com/company/game/EnumData.java
@@ -66,4 +66,82 @@ public class EnumData {
             this.failureChance = failureChance;
         }
     }
+
+    public enum VechicleBrands {
+//        zbyt wiele danych do obrobienia częśćmarek usuniętych w obecnej wersji
+//        ALFAROMEO(true, false, false),
+//        ASTONMARTIN(true,false,false),
+        AUDI(true,false,false,94500.0,null,"premium",null,1.5),
+//        BENTLEY(true, false, false),
+        BMW(true, true, false,91950.0,null,"premium", null,1.6),
+//        CHEVROLET(true,false,false),
+        CITROEN(true,false,true,30000.0,41750.00, "standard", 1390.0,1.2),
+        DACIA(true,false,true,25000.00, 32250.00, "budget",1200.0, 0.6),
+//        DODGE(true,false,false),
+        FERRARI(true, false, false, 375000.0, null, "premium", null, 2.5),
+        FIAT(true,false,true, 30500.0,50500.0,"budget", 1187.0,0.7),
+        FORD(true,false,true,46500.0,40000.0,"standard",1500.0,0.9),
+        HONDA(true, true,false,55500.0,null,"standard",null,1.3),
+//        HUNDAI(true,false,true),
+        INFINITI(true, false, false,74000.0,null,"premium",null,1.7),
+//        IVECO(false,false,true),
+//        IZERA(true, false, false),
+//        JAGUAR(true, false, false),
+//        JEEP(true, false, false),
+//        KAMAZ(false,false,true),
+//        KAWASAKI(false,true,false),
+//        KIA(true,false,true),
+//        LAMBORGINI(true, false, false),
+//        LANCIA(true, false, fals
+        MAZDA(true, false, true,47500.0,null,"standard",null,1.25),
+        MERCEDES(true, false, true,630000.0,60000.0,"premium",2300.0,1.9),
+        MINI(true, false, false,42500.0,null,"standard", null, 1.4),
+        MITSUBISHI(true, false, true,74000.0,40000.0, "standard", 1100.0,1.3),
+        NISSAN(true, false, true, 48000.0,39250.0,"standard", 680.0,1.35),
+        OPEL(true, false, true,43500.0,26950.0,"budget",1323.0,0.8),
+        PEUGEOT(true, false, true,29500.00,51250.0,"budget",1300.0,1.15),
+        PORSCHE (true, false, false,312500.0, null,"premium", null, 1.8),
+        RENAULT(true, false, true,35500.0,46750.0,"standard",1257.0,1.15),
+//        ROVER (true, false, false),
+        SEAT(true, false, true,35000.0,null,"standard",null,0.9),
+        SKODA(true,false,true,31750.0,27450.0,"budget",560.0,0.8),
+//        SUBARU(true, false, false),
+//        SUZUKI(true, true, false,),
+//        TESLA (true, false, false),
+        TOYOTA(true, false, true,43500.0,58000.0,"standard",1100.0,1.35),
+        VOLKSWAGEN(true, false, true, 65500.0,45500.0,"standard",1428.0,1.5);
+//        VOLVO(true, false, true);
+
+        boolean isCar;
+        boolean isMotorcycle;
+        boolean isCargo;
+        Double mediumPricePersonal;
+        Double mediumPriceCargo;
+        String segment;
+        Double cargoCapacity;
+        double partsBrandMultiplier;
+
+        VechicleBrands(boolean isCar, boolean isMotorcycle, boolean isCargo, Double mediumPricePersonal, Double mediumPriceCargo, String segment, Double cargoCapacity, double partsBrandMultiplier) {
+            this.isCar = isCar;
+            this.isMotorcycle = isMotorcycle;
+            this.isCargo = isCargo;
+            this.mediumPricePersonal = mediumPricePersonal;
+            this.mediumPriceCargo = mediumPriceCargo;
+            this.segment = segment;
+            this.cargoCapacity = cargoCapacity;
+            this.partsBrandMultiplier = partsBrandMultiplier;
+        }
+    }
+
+    public enum CustomerVechicleCondition {
+        OPERATIONAL(80),
+        FAULTYSUSPENSION(15),
+        BROKEN(5);
+
+        int buyingVillingness;
+
+        CustomerVechicleCondition(int buyingVillingness) {
+            this.buyingVillingness = buyingVillingness;
+        }
+    }
 }
